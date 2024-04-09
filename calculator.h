2e1c55258d3f0fe7ac1227ff287d212edb49c42f
Kieran-C <kieran.caruana@gmail.com>
2024-04-09 22:19:27 +0100

    Further work on calculator, conversion still needs corrected

--- calculator.h
+++ calculator.h
@@ -2,27 +2,63 @@
 #include <stdlib.h>
 #include <string.h>
 
+static double runCalculation(double value1, double value2, char* operation) {
+    printf("num1: %f num2: %f, op: %s", value1, value2, operation);
+    double result;
+    switch (*operation) {
+        case '+':
+            printf("plus");
+            result = (value1 + value2);
+            return result;
+        case '-':
+            printf("minus");
+            result = (value1 - value2);
+            return result;
+        case '*':
+            printf("multiply");
+            result = (value1 * value2);
+            return result;
+        case '/':
+            printf("divide");
+            result = (value1 / value2);
+            return result;
+    }
+}
+
 static double calculate(char *equation) {
-    char value1;
+    char *value1;
     char value2;
     char *operation;
 
+    //This splits up the equation into individual elements
     int counter = 1;
     for (char *p = strtok(equation," "); p != NULL; p = strtok(NULL, " ")){
-        puts(p); //for dubugging purposes
+        printf("loop: p: %s", p);
         switch (counter) {
             case 1:
-                value1 = atof(p);
+                printf("here");
+                strcpy(value1, p);
+                printf("here23123 val1: %s", value1);
+                //define char array here and then move it out
             case 2:
                 strcpy(operation, p);
             case 3:
-                value2 = atof(p);
+                strcpy(value2, p);
         }
         counter++;
     }
 
-    printf("V1: %d, operator: %s, V2: %d", value1, operation, value2);
-    return 0;
+    printf("here2");
+
+    double decVal1 = atof(value1);
+    double decVal2 = atof(value2);
+
+    double result = runCalculation(decVal1, decVal2, operation);
+
+    printf("\n----------------------------\n");
+    printf("Answer: %d", result);
+
+    return result;
 }
 
 int initCalculator() {
