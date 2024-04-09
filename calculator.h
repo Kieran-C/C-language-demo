2fba8e5a6f837e8da7b4dee250fec6ae06f29b38
Kieran-C <kieran.caruana@gmail.com>
2024-04-09 02:04:08 +0100

    first commit

---
+++ calculator.h
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static double calculate(char *equation) {
+    char value1;
+    char value2;
+    char *operation;
+
+    int counter = 1;
+    for (char *p = strtok(equation," "); p != NULL; p = strtok(NULL, " ")){
+        puts(p); //for dubugging purposes
+        switch (counter) {
+            case 1:
+                value1 = atof(p);
+                // value1 = p[1];
+            case 2:
+                strcpy(operation, p);
+            case 3:
+                value2 = atof(p);
+                // value2 = p[1];
+        }
+        counter++;
+    }
+
+    printf("V1: %d, operator: %s, V2: %d", value1, operation, value2);
+    return 0;
+}
+
+int initCalculator() {
+    char equation[100];
+
+    printf("In the format of number space operator space number, eg. 3 + 5\nPlease enter your calculation: ");
+    scanf("%[^\n]", &equation);
+
+    printf("%s ", equation);
+
+    double result = calculate(equation);
+
+    return 0;
+}
---
+++ launcher.c
@@ -0,0 +1,16 @@
+// Header file for input output functions
+#include <stdio.h>
+#include <stdlib.h>
+//Inlcude custome headers
+#include "calculator.h"
+
+// main function -
+// where the execution of program begins
+int main()
+{
+
+	// prints welcome message
+	printf("Welcome to Kierans C Language Demo Program\n");
+	initCalculator();
+	return 0;
+}
